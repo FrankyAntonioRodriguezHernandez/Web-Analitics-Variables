ac4270d9b74173becba68494758ff10032755185
franky <frankyan2001@gmail.com>
2025-03-06 10:40:15 -0500

    Creando la Entity WebsiteDescription

---
+++ entity/WebsiteDescription.java
@@ -0,0 +1,42 @@
+import com.fasterxml.jackson.annotation.JsonView;
+import com.vladmihalcea.hibernate.type.json.JsonStringType;
+import cu.redcuba.controller.api.views.ApiView;
+import org.hibernate.annotations.*;
+import org.hibernate.type.StandardBasicTypes;
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "d_website_description")
+@TypeDef(name = "json", typeClass = JsonStringType.class)
+@DynamicInsert
+@DynamicUpdate
+@NamedQueries({
+    @NamedQuery(name = "WebsiteDescription.findAll", query = "SELECT wd FROM WebsiteDescription wd")
+})
+public class WebsiteDescription implements Serializable {
+
+private static final long serialVersionUID = 1L;
+
+@EmbeddedId
+protected WebsiteDescriptionPK pk;
+
+@Basic(optional = false)
+@Type(type = "json")
+@Column(name = "description", columnDefinition = "json")
+private String description;
+
+@Basic(optional = false)
+@Column(name = "length")
+private int length;
+
+@Column(name = "created", insertable = false, updatable = false)
+@Temporal(TemporalType.TIMESTAMP)
+private Date created;
+
