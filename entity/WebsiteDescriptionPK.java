d8970e2dc0d104bdbd0df8f630f882cbeac59482
franky <frankyan2001@gmail.com>
2025-03-07 23:02:15 -0500

    creando la entity WebsiteDescriptionPK

---
+++ entity/WebsiteDescriptionPK.java
@@ -0,0 +1,13 @@
+package cu.redcuba.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.*;
+
+@Embeddable
+public class WebsiteDescriptionPK implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    
+}
