31c68685f52ea2be5cc87aafbf8af17e8252101f
Franky <frankyan2001@gmail.com>
2025-03-10 12:00:00 -0400

    Creando el WebsiteDescriptionFactory

---
+++ factory/WebsiteDescriptionFactory.java
@@ -0,0 +1,14 @@
+package cu.redcuba.factory;
+import cu.redcuba.entity.WebsiteDescription;
+import cu.redcuba.repository.WebsiteDescriptionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class WebsiteDescriptionFactory {
+
+}
