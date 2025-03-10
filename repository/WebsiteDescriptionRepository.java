352538ee5b1f6768e019337966121dfcadbed112
Franky <frankyan2001@gmail.com>
2025-03-10 12:00:00 -0400

    arreglando el WebsiteDescriptionRepository

--- repository/WebsiteDescriptionRepository.java
+++ repository/WebsiteDescriptionRepository.java
@@ -1,14 +1,13 @@
 package cu.redcuba.repository;
 import cu.redcuba.entity.WebsiteDescriptionPK;
 import cu.redcuba.entity.WebsiteDescription;
-import cu.redcuba.entity.WebsiteLink;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Date;
 import java.util.List;
 
-public interface WebsiteDescriptionRepository {
+public interface WebsiteDescriptionRepository extends JpaRepository<WebsiteDescriptionPK, WebsiteDescription>{
 
     @Query(
             value = "SELECT MAX(wd.pk) FROM WebsiteDescription wd"
