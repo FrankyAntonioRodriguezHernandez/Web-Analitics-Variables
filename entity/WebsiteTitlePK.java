60fb5a8f2857df330532b3ba8ec2104593416dff
Franky <frankyan2001@gmail.com>
2025-03-11 12:00:00 -0400

    Creando WebsiteTitlePK

---
+++ entity/WebsiteTitlePK.java
@@ -0,0 +1,15 @@
+package cu.redcuba.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.*;
+
+public class WebsiteTitlePK implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    @Column(name = "website_id", nullable = false)
+    protected Long websiteId;
+
+    @Column(name = "day", nullable = false)
+    protected String day;
+}
